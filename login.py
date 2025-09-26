3be88411ff3aaee6093620046fefeca5dfd38bf5
Isadora Sousa Silva <isadora.cd13@gmail.com>
2025-09-26 19:14:42 -0300

    Deu certo

---
+++ login.py
@@ -0,0 +1,14 @@
+usuario = input ('Digite seu usuário: ')
+senha = int( input ('Digite a sua senha: '))
+cont = 2
+
+while usuraio != 'admin' or senha != 123:
+	print ('Usuário ou senha inválidos. Tente novamente.')
+	usuario = input ('Digite seu usuário.')
+	senha = int (input ('Digite sua senha')
+	cont = cont - 1
+	if cont == 0:
+		break
+	else:
+		print ('Acesso liberado.')
+
