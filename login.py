8a256c98faa15c852d151bee6cdd6656e34380d2
Isadora Sousa Silva <isadora.cd13@gmail.com>
2025-09-26 19:28:14 -0300

    Pequenas alteracoes

--- login.py
+++ login.py
@@ -2,13 +2,12 @@ usuario = input ('Digite seu usuário: ')
 senha = int( input ('Digite a sua senha: '))
 cont = 2
 
-while usuraio != 'admin' or senha != 123:
+while usuario != 'admin' or senha != 123:
 	print ('Usuário ou senha inválidos. Tente novamente.')
-	usuario = input ('Digite seu usuário.')
-	senha = int (input ('Digite sua senha')
+	usuario = input ('Digite seu usuário: ')
+	senha = int(input ('Digite sua senha: '))
 	cont = cont - 1
 	if cont == 0:
 		break
 	else:
 		print ('Acesso liberado.')
-
